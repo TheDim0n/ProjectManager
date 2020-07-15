495512d7090122adeffc30e3e8847192af4cc144
TheDim0n <dim0n2023@yandex.ru>
2020-07-15 20:55:00 +0300

    add status list view

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('statuses', views.status_list, name='status_list'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Status
 
 
 def index(request):
     return HttpResponse('Works!')
+
+def status_list(request):
+    __list = Status.objects.all()
+    context = {
+        'status_list': __list,
+    }
+    return render(request, 'tasks/statuses.html', context)
