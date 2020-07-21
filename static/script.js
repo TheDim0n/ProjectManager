9eaee61b3e57eabbe4822cd17c2bc01e7cf2584f
Nekolya <Nekolya@users.noreply.github.com>
2020-07-21 22:15:27 +0300

    test static

---
+++ static/script.js
@@ -0,0 +1,9 @@
+function greetMe(yourName) {
+    alert('Hello ' + yourName);
+  }
+  console.log(eval('3 + 5'));
+
+// $('.navbar-nav .nav-link').click(function(){
+//     $('.navbar-nav .nav-link').removeClass('active');
+//     $(this).addClass('active');
+// })
