c59bc4b39285065477b2163d68a131e1095c716d
Nekolya <Nekolya@users.noreply.github.com>
2020-07-22 13:19:51 +0300

    Js test

--- static/script.js
+++ static/script.js
@@ -1,9 +1,4 @@
-function greetMe(yourName) {
-    alert('Hello ' + yourName);
-  }
-  console.log(eval('3 + 5'));
-
-// $('.navbar-nav .nav-link').click(function(){
-//     $('.navbar-nav .nav-link').removeClass('active');
-//     $(this).addClass('active');
-// })
+$('.navbar-nav .nav-link').click(function(){
+    $('.navbar-nav .nav-link').removeClass('active');
+    $(this).addClass('active');
+})
