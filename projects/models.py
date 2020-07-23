eb7eca9679d7069a0f9fced43905ea3822e91aad
Olya <olya75825@gmail.com>
2020-07-23 21:52:47 +0300

    Models changes

--- projects/models.py
+++ projects/models.py
@@ -1,20 +1,19 @@
 from django.db import models
-#from tasks.models import Status
+from status.models import Status
 
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
-    # status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True, editable = False)
 
     def __str__(self):
         return self.name
 
 
-
 class Level(models.Model):
     name = models.CharField(max_length=100)
     project = models.ForeignKey(Project, on_delete=models.PROTECT, null=True)
-    # status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True, editable = False)
 
     def __str__(self):
         return self.name
--- tasks/models.py
+++ tasks/models.py
@@ -5,21 +5,20 @@ from django.utils import timezone
 from django.urls import reverse
 
 from status.models import Status
-# from projects.models import Level
+from projects.models import Level
 
 
 class Task(models.Model):
-
     name = models.CharField(max_length=100)
     start_date = models.DateField(default=timezone.now())
     finish_date = models.DateField(default=timezone.now() + datetime.timedelta(days=1))
     status = models.ForeignKey(
         Status,
         on_delete=models.PROTECT,
-        default=Status.objects.get_or_create(text="No status"),
+        default=Status.objects.get_or_create(text="No status")[0].id,
     )
     description = models.TextField(max_length=1000, blank=True)
-    # level = models.ForeignKey(Level, on_delete=models.PROTECT, null=True)
+    level = models.ForeignKey(Level, on_delete=models.PROTECT, null=True, blank = True, editable = False)
 
     def __str__(self):
         return self.name
