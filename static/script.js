0e6037c60ef94b47aef6a00a62bca5b1b8e208f7
Olya <olya75825@gmail.com>
2020-08-05 21:54:08 +0300

    Remove reapiting and add js to icons

--- static/script.js
+++ static/script.js
@@ -5,4 +5,11 @@ $(document).ready(function() {
     if(b == "users") b = (a).split("/")[4];
     console.log(b);
     if(b) $("." + b).addClass('active');
+    $('.zero-card').hover(
+        function(){
+          $(this).find(".icons").removeClass("invisible");
+        },
+        function(){
+            $(this).find(".icons").addClass("invisible");
+        });
 });
